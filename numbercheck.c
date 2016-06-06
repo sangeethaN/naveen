377896c363827fd17d9d24ec094772207aa95781
sangeethaN <sangenandhu@gmail.com>
2016-06-06 22:32:30 +0530

    Rename number.c to numbercheck.c

---
+++ numbercheck.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int number;
+scanf("%d",&number);
+if(number>0)
+printf("THE NUMBER IS A POSITIVE NUMBER");
+if(number<0)
+printf("THE NUMBER IS A NEGATIVE NUMBER");
+if(number==0)
+printf("THE NUMBER IS ZERO");
+return 0;
+}
