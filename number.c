5c518a9d114dd0aa1b1eb43b7a2f3a9ffd7aada3
sangeethaN <sangenandhu@gmail.com>
2016-06-06 11:57:53 +0530

    Update and rename .gitignore to number.c

---
+++ number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+printf("the nuber is positive number:);
+if(n<0)
+printf("the nuber is negative number:);
+if(n==0)
+printf("the number is zero");
+return 0;
+}
